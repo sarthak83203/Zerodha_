8c0cad1f830e5619c6dba4b6b2fe1f79edd0d953
sarthak <sarthak.shinde2006@gmail.com>
2026-02-21 12:23:31 +0530

    Hero Section created

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,8 +1,20 @@
 import React from 'react';
 function Hero() {
+    //ok like if img is in the src then we should import like {}...
+    
     return ( 
-        <h1>Hero <i class="fa fa-user" aria-hidden="true"></i>
-</h1>
+        <div className='container p-5  mb-5'>
+            <div className='row text-center'>
+                
+                <img src='media/images/homeHero.png' alt='Hero_Image' className='mb-5'/>
+                <h1 className='mt-5'>Invest in everything</h1>
+                <p>Online platform to invest in stocks,derivatives,mutual funds,and more</p>
+                <button className='p-3 btn btn-primary fs-5 mb-5' style={{width:"25%",margin:"0 auto"}}>SignUp Now</button>
+            </div>
+
+
+        </div>
+
      );
 }
 
