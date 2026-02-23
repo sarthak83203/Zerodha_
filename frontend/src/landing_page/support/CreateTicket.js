73038f65aa34cc28e5c450fd30d3967af909c7f8
sarthak <sarthak.shinde2006@gmail.com>
2026-02-23 11:44:41 +0530

    creating Ticket

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,7 +1,67 @@
 import React from 'react';
 function CreateTicket() {
     return ( 
-        <h1>CreateTicket</h1>
+        <div className='container'>
+            <div className='row p-5 mt-5 mb-5'>
+                <h1 className='fs-2 '>To create a ticket,Select a relevent topic </h1>
+                 <div className='col-4 p-5 mt-5 mb-3'>
+                <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i>Account Opening</h4>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Online Account Opening</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Offline Account Opening</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Company,Patnership anf HUF Account</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Opening</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>NRI Account Opening</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Charges at Zerodha</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Zerodha IDFC FIRST Bank 3-in-1 Account</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Getting Started</a><br></br>
+                </div>
+                    <div className='col-4 p-5 mt-5 mb-3'>
+                <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i> Console</h4>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Portfolio</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Corporate actions</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Funds statement</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Reports</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Profile</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Segments</a><br></br>
+               
+                </div>
+                 <div className='col-4 p-5 mt-5 mb-3'>
+                <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i> Coin</h4>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Track accountopening</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Track segment activation</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Intraday margins</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Kite user manual</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Learn how to create a ticket</a><br></br>
+               
+                </div>
+                 <div className='col-4 p-5 mt-5 mb-3'>
+                <h4 className=''><i class="fa fa-plus-circle" aria-hidden="true"></i> Kite</h4>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>IPO</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Trading FAQs</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Margin Trading Facility (MTF) and Margins</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Charts and orders</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Alerts and Nudges</a><br></br>
+                  <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>General</a><br></br>
+               
+                </div>
+                <div className='col-4 p-5 mt-5 mb-3'>
+                <h4 className=''><i class="fa fa-inr" aria-hidden="true"></i> Funds</h4>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Add money</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Withdraw money</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Add bank accounts</a><br></br>
+                  <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>eMandates</a><br></br>
+                </div>
+                 <div className='col-4 p-5 mt-5 mb-3'>
+                <h4 className=''><i class="fa fa-user-circle-o" aria-hidden="true"></i>  Your Zerodha Account</h4>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Your Profile</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Account modification</a><br></br>
+                 <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Client Master Report (CMR) and Depository Participant (DP)</a><br></br>
+                  <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Nomination</a><br></br>
+                  <a href='' className='mt-5' style={{textDecoration:"none",lineHeight:"2.5"}}>Transfer and conversion of securities</a><br></br>
+                </div>
+                
+            </div>
+        </div>
      );
 }
 
