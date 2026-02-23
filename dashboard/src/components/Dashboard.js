9bd49429ca3b0809d71d728df754cb472602bc45
sarthak <sarthak.shinde2006@gmail.com>
2026-02-23 23:15:40 +0530

    dashboard created

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -24,7 +24,7 @@ const Dashboard = () => {
           <Route path="/holdings" element={<Holdings />} />
           <Route path="/positions" element={<Positions />} />
           <Route path="/funds" element={<Funds />} />
-          <Route path="/apps" element={<Apps />} />
+          
         </Routes>
       </div>
     </div>
