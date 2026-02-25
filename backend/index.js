7b3f4f92d18a39cd3e2976a158e6e350fe016ab7
sarthak <sarthak.shinde2006@gmail.com>
2026-02-25 23:11:53 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,54 @@ const express=require("express");
 const connectDb = require("./config/db");
 const { holding } = require("./model/HoldingsModel");
 const { position } = require("./model/Position");
+const mongoose=require("mongoose");
+const bodyParser=require("body-parser");
+const cors=require("cors");
+const { order } = require("./model/Order");
+
 require("dotenv").config();
+
 
 const app=express();
 const PORT=process.env.PORT ||3004;
+app.use(cors());
+app.use(bodyParser.json());
+
+//End point
+//By this endpoints i can connect to dashboard that is part of frontend
+app.get("/holdings",async(req,res)=>{
+    //reading data
+    //sending to frontend
+    let holdings=await holding.find({});
+    res.json(holdings);
+
+
+
+})
+
+app.get("/orders",async(req,res)=>{
+    let orders=await order.find({});
+    res.json(orders);
+})
+app.get("/positions",async(req,res)=>{
+    //reading data
+    //sending to frontend
+    let positions=await position.find({});
+    res.json(positions);
+})
+
+app.post("/newOrder",async(req,res)=>{
+    let newOrder=new order({
+    name:req.body.name,
+    qty:req.body.qty,
+    price:req.body.price,
+    mode:req.body.mode,
+    });
+    newOrder.save();
+    res.send("saved");
+
+
+})
 
 app.listen(PORT,()=>{
     console.log("App Started");
